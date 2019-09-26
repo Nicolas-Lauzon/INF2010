78a728da08e354d3c088c009b0590b4f82dc1e14
Nicolas.Lauzon <lauzon.n.pp@gmail.com>
2019-09-26 16:11:58 -0400

    avancement BaseShape

--- labo/src/tp1/BaseShape.java
+++ labo/src/tp1/BaseShape.java
@@ -10,49 +10,60 @@ public class BaseShape {
 
     // TODO Initialiser les points.
     public BaseShape() {
-        Set<Point2d> coords = new HashSet<Point2d>(2);
+        this.coords = new HashSet<Point2d>();
     }
 
     // TODO prendre une liste de points et creer une nouvelle forme.
     public BaseShape(Collection<Point2d> coords) {
-       //
+       this.coords= new HashSet<Point2d>();
+       this.coords.addAll(coords);
     }
 
     // TODO ajouter ou retirer des coordonnees a la liste de points.
     public void add(Point2d coord) {
-        coords.add(coord);
+        this.coords.add(coord);
     }
     public void add(BaseShape shape) {
-        coords.addAll(shape.coords);
+        this.coords.addAll(shape.coords);
     }
     public void addAll(Collection<Point2d> coords) {
-
+        this.coords.addAll(coords);
     }
     public void remove(Point2d coord) {
-        coords.remove(coord);
+        this.coords.remove(coord);
     }
     public void remove(BaseShape shape) {
-        coords.removeAll(shape.coords);
+        this.coords.removeAll(shape.coords);
     }
     public void removeAll(Collection<Point2d> coords) {
-        // ...
+        this.coords.removeAll(coords);
     }
 
     // TODO retourne les coordonnees de la liste.
     public Set<Point2d> getCoords() {
+        return (new HashSet<Point2d>(this.coords));
 
-        return coords;
     }
 
     // TODO appliquer la translation sur la forme.
     public BaseShape translate(Point2d point) {
+       /* BaseShape temp = new BaseShape();
+        Iterator iterator = this.coords.iterator();
+        while (iterator.hasNext()){
+            Point2d x = (Point2d) iterator.next();
+            x.translate(point);
+            temp.add(x);
+        }*/
+       return (new BaseShape(translateAll(point)));
 
-        return null;//point.translate(point) ;
     }
 
     // TODO appliquer la translation sur la liste.
     public Set<Point2d> translateAll(Point2d point) {
-        return null;
+        Set<Point2d> temp = new HashSet<Point2d>();
+        temp.addAll(this.coords);
+        temp.forEach((e) -> {e.translate(point);});
+        return temp;
     }
 
     // TODO appliquer la rotation sur la forme.
@@ -67,6 +78,6 @@ public class BaseShape {
 
     // TODO retourner une nouvelle forme.
     public BaseShape clone() {
-        return this;
+        return (new BaseShapethis);
     }
 }
